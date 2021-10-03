c90c88522e01f7f297f72cbfa17f1d542fd295cb
Shubham Singh <shubham88fru@yahoo.com>
2021-10-03 11:46:22 +0530

    init and reset

--- script.js
+++ script.js
@@ -13,15 +13,30 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -53,17 +68,21 @@ btnHold.addEventListener('click', function onBtnHoldClick() {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
       document
-        .querySelector(`player--${activePlayer}`)
-        .classList.remove('.player--active');
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
     } else {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function onBtnNewClick() {
+  init();
 });
